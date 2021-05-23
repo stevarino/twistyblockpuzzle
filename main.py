2765f633b765e696f98706c909c98c12357f1c49
Stephen O'Connor <>
2021-05-23 09:48:46 +0000

    Change fill_state to a dictionary, position/direction to tuples

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 pieces = [3,1,1,2,1,2,1,1,2,2,1,1,1,2,2,2,2]
 assert sum(pieces) == (3 * 3 * 3)
 
-# Build a 3x3x3 array filled with zeros
-initial_fill_state = [[[0] * 3] * 3] * 3
-
 class Step:
   """A "step" in solving the puzzle, spawning subsequent child steps"""
   def __init__(self, index, pos, direction, fill_state):
     # index - what piece we're on (0 is the first piece...)
     self.index = index
 
-    # pos - position, where is the "head" of the snake [x, y, z]
+    # pos - position, where is the "head" of the snake (x, y, z)
     self.pos = pos
 
-    # direction - which way are we going? [1,0,0] is positive x
+    # direction - which way are we going? (1,0,0) is positive x
     self.direction = direction
 
-    # fill_state - what blocks in the cube are already filled?
+    # fill_state - what blocks in the cube are already filled? A {[coords] => bool} mapping
     self.fill_state = fill_state
 
   def next(self):
@@ -33,14 +30,12 @@ class Step:
     
     # next, lets update fill_state, checking that its not already filled
     for i in piece_len:
-      self.pos = [self.pos[i] + self.direction[i] for i in range(3)]
+      self.pos = tuple(self.pos[i] + self.direction[i] for i in range(3))
       # check that the currnt value is 0
-      focus = self.fill_state
-      for val in self.pos[0:-1]:  # all but the last last dimension (z)
-        focus = focus[val]
-      if focus[self.pos[-1]] == 1:  # now we read the last dimension...
-        # we've focused on a filled square... we're in an illegal state
+      if self.fill_state.get(self.pos):
         return []
       # mark the box as filled
-      focus[self.pos[-1]] = 1
+      self.fill_state[self.pos] = True
     
+    # And if we made it this far, we know now that the Step is valid
+
