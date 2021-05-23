56f9c9de4c3dce7f55bf6972481d08360ae67ac4
Stephen O'Connor <>
2021-05-23 08:58:22 +0000

    Add Step class with relevant state

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 pieces = [3,1,1,2,1,2,1,1,2,2,1,1,1,2,2,2,2]
 assert sum(pieces) == (3 * 3 * 3)
+
+class Step:
+  def __init__(self, index, pos, direction, fill_state):
+    # index - what piece we're on (0 is the first piece...)
+    self.index = index
+
+    # pos - where is the "head" of the snake [x, y, z]
+    self.pos = pos
+
+    # direction - which way are we going? [1,0,0] is positive x
+    self.direction = direction
+
+    # fill_state - what blocks in the cube are already filled?
+    self.fill_state = fill_state
